6293793c1b01cdde5366571b906ec9649daca09e
xPolar <polar@polar.blue>
2024-12-28 14:24:00 -0800

    feat: deeper invite logging

--- src/bot/events/guildMemberAdd.ts
+++ src/bot/events/guildMemberAdd.ts
@@ -1,4 +1,7 @@
-import type { GatewayGuildMemberAddDispatchData, WithIntrinsicProps } from "@discordjs/core";
+import type {
+	GatewayGuildMemberAddDispatchData,
+	WithIntrinsicProps
+} from "@discordjs/core";
 import { GatewayDispatchEvents } from "@discordjs/core";
 import { DiscordSnowflake } from "@sapphire/snowflake";
 import EventHandler from "../../../lib/classes/EventHandler.js";
@@ -14,41 +17,58 @@ export default class GuildMemberAdd extends EventHandler {
 	 *
 	 * https://discord.com/developers/docs/topics/gateway-events#guild-member-add
 	 */
-	public override async run({ data: member }: WithIntrinsicProps<GatewayGuildMemberAddDispatchData>) {
+	public override async run({
+		data: member
+	}: WithIntrinsicProps<GatewayGuildMemberAddDispatchData>) {
 		this.client.approximateUserCount++;
 
-		this.client.dataDog.increment("guild_members", 1, [`guildId:${member.guild_id}`]);
+		this.client.dataDog.increment("guild_members", 1, [
+			`guildId:${member.guild_id}`
+		]);
 
-		const newInvites = await this.client.api.guilds.getInvites(member.guild_id);
+		const newInvites = await this.client.api.guilds.getInvites(
+			member.guild_id
+		);
 
 		if (!this.client.invitesCache.has(member.guild_id))
-			this.client.invitesCache.set(member.guild_id, new Map(newInvites.map((invite) => [invite.code, invite.uses])));
+			this.client.invitesCache.set(
+				member.guild_id,
+				new Map(newInvites.map((invite) => [invite.code, invite.uses]))
+			);
 
 		const invitesCache = this.client.invitesCache.get(member.guild_id)!;
-		const usedInvite = newInvites.find((invite) => invite.uses > (invitesCache?.get(invite.code) ?? 0));
+		const usedInvite = newInvites.find(
+			(invite) => invite.uses > (invitesCache?.get(invite.code) ?? 0)
+		);
 
 		if (usedInvite) {
-			this.client.dataDog.increment("guild_joins", 1, [`guildId:${member.guild_id}`, `invite:${usedInvite.code}`]);
+			this.client.dataDog.increment("guild_joins", 1, [
+				`guildId:${member.guild_id}`,
+				`invite:${usedInvite.code}`
+			]);
 
 			invitesCache.set(usedInvite.code, usedInvite.uses);
 			this.client.invitesCache.set(member.guild_id, invitesCache);
-		} else this.client.dataDog.increment("guild_joins", 1, [`guildId:${member.guild_id}`]);
+		} else
+			this.client.dataDog.increment("guild_joins", 1, [
+				`guildId:${member.guild_id}`
+			]);
 
 		const [loggingChannels] = await Promise.all([
 			this.client.prisma.logChannel.findMany({
 				where: {
 					event: "MEMBER_JOINED",
-					guildId: member.guild_id,
-				},
+					guildId: member.guild_id
+				}
 			}),
 			this.client.prisma.memberJoin.create({
 				data: {
 					guildId: member.guild_id,
 					joinedAt: new Date(),
 					userId: member.user!.id,
-					inviteCode: usedInvite?.code ?? null,
-				},
-			}),
+					inviteCode: usedInvite?.code ?? null
+				}
+			})
 		]);
 
 		if (!loggingChannels.length) return;
@@ -57,15 +77,15 @@ export default class GuildMemberAdd extends EventHandler {
 			this.client.prisma.message.count({
 				where: {
 					guildId: member.guild_id,
-					authorId: member.user!.id,
-				},
+					authorId: member.user!.id
+				}
 			}),
 			this.client.prisma.memberJoin.count({
 				where: {
 					guildId: member.guild_id,
-					userId: member.user!.id,
-				},
-			}),
+					userId: member.user!.id
+				}
+			})
 		]);
 
 		return Promise.all(
@@ -75,26 +95,49 @@ export default class GuildMemberAdd extends EventHandler {
 						{
 							author: {
 								name: "Member Joined",
-								icon_url: "https://cdn.karalite.com/user_2Z1iHO0Px2py0XMFB2sy1mb631L/auditMemberCreate.png",
+								icon_url:
+									"https://cdn.karalite.com/user_2Z1iHO0Px2py0XMFB2sy1mb631L/auditMemberCreate.png"
 							},
 							description: `**User:** <@${member.user!.id}> \`[${
 								member.user!.id
-							}]\`\n**Account Created:** ${this.client.functions.generateTimestamp({
-								timestamp: DiscordSnowflake.timestampFrom(member.user!.id),
-							})} (${this.client.functions.generateTimestamp({
-								timestamp: DiscordSnowflake.timestampFrom(member.user!.id),
-								type: "R",
-							})})\n${previousJoinCount > 1 ? `\n**Previous Join Count:** ${previousJoinCount.toLocaleString()}` : ""}${
-								previousMessageCount > 1 ? `\n**Previous Message Count:** ${previousMessageCount.toLocaleString()}` : ""
-							}${usedInvite ? `\n**Invite:** https://discord.gg/${usedInvite.code}` : ""}`,
-							color: this.client.config.colors.success,
-						},
+							}]\`\n**Account Created:** ${this.client.functions.generateTimestamp(
+								{
+									timestamp: DiscordSnowflake.timestampFrom(
+										member.user!.id
+									)
+								}
+							)} (${this.client.functions.generateTimestamp({
+								timestamp: DiscordSnowflake.timestampFrom(
+									member.user!.id
+								),
+								type: "R"
+							})})\n${
+								previousJoinCount > 1
+									? `\n**Previous Join Count:** ${previousJoinCount.toLocaleString()}`
+									: ""
+							}${
+								previousMessageCount > 1
+									? `\n**Previous Message Count:** ${previousMessageCount.toLocaleString()}`
+									: ""
+							}${
+								usedInvite
+									? `\n**Invite:** https://discord.gg/${
+											usedInvite.code
+									  }${
+											usedInvite.inviter?.id
+												? `created by <@${usedInvite.inviter.id}> \`[${usedInvite.inviter.id}]\``
+												: ""
+									  } with ${usedInvite.uses} uses`
+									: ""
+							}`,
+							color: this.client.config.colors.success
+						}
 					],
 					allowed_mentions: {
-						parse: [],
-					},
-				}),
-			),
+						parse: []
+					}
+				})
+			)
 		);
 	}
 }
